3adae0830477597410a17d9dc5255abe6b7bce79
Sweetybhonde <sweetymbhonde@gmail.com>
2023-02-23 19:55:14 +0530

    foodorder.repository

---
+++ repository/ItemRepo.java
@@ -0,0 +1,8 @@
+package com.food.foodSpringApp.repository;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.food.foodSpringApp.dto.Item;
+
+public interface ItemRepo extends JpaRepository<Item,Integer>{
+
+}
